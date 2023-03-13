ea0fbbf0326cd33d7976849da72a8a1033b6345c
Damlrca <sir.damirca@yandex.ru>
2023-03-13 17:21:20 +0300

    init

---
+++ ComputerGraphics_TomogramVisualizer/Form1.cs
@@ -0,0 +1,10 @@
+namespace ComputerGraphics_TomogramVisualizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
