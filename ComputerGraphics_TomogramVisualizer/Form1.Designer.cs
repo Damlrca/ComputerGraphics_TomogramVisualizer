af91e6718042ce5b17563cd1f03b3831d9e50303
Damlrca <sir.damirca@yandex.ru>
2023-03-13 17:52:40 +0300

    add glControl

--- ComputerGraphics_TomogramVisualizer/Form1.Designer.cs
+++ ComputerGraphics_TomogramVisualizer/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ComputerGraphics_TomogramVisualizer
+﻿using OpenTK.WinForms;
+
+namespace ComputerGraphics_TomogramVisualizer
 {
     partial class Form1
     {
@@ -28,12 +30,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.glControl1 = new OpenTK.WinForms.GLControl();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // glControl1
+            // 
+            this.glControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.glControl1.API = OpenTK.Windowing.Common.ContextAPI.OpenGL;
+            this.glControl1.APIVersion = new System.Version(3, 3, 0, 0);
+            this.glControl1.Flags = OpenTK.Windowing.Common.ContextFlags.Default;
+            this.glControl1.IsEventDriven = true;
+            this.glControl1.Location = new System.Drawing.Point(12, 27);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Profile = OpenTK.Windowing.Common.ContextProfile.Compatability;
+            this.glControl1.Size = new System.Drawing.Size(379, 211);
+            this.glControl1.TabIndex = 2;
+            this.glControl1.Text = "glControl1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.glControl1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private GLControl glControl1;
     }
 }
