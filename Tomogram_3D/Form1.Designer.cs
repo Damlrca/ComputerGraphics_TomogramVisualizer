121053c710343f0c3db71cb5fc78d5e5cc2c773d
Damlrca <sir.damirca@yandex.ru>
2023-03-31 13:41:46 +0300

    new form for Tomogram_3D

---
+++ Tomogram_3D/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Tomogram_3D
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            glControl1 = new OpenTK.WinForms.GLControl();
+            SuspendLayout();
+            // 
+            // glControl1
+            // 
+            glControl1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            glControl1.API = OpenTK.Windowing.Common.ContextAPI.OpenGL;
+            glControl1.APIVersion = new System.Version(3, 3, 0, 0);
+            glControl1.Flags = OpenTK.Windowing.Common.ContextFlags.Default;
+            glControl1.IsEventDriven = true;
+            glControl1.Location = new System.Drawing.Point(114, 51);
+            glControl1.Name = "glControl1";
+            glControl1.Profile = OpenTK.Windowing.Common.ContextProfile.Compatability;
+            glControl1.Size = new System.Drawing.Size(504, 400);
+            glControl1.TabIndex = 3;
+            glControl1.Text = "glControl1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(732, 503);
+            Controls.Add(glControl1);
+            MinimumSize = new System.Drawing.Size(750, 550);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private OpenTK.WinForms.GLControl glControl1;
+    }
+}
---
+++ Tomogram_Utilities/Bin3D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomogram_Utilities
+{
+    public static class Bin3D
+    {
+
+    }
+}
